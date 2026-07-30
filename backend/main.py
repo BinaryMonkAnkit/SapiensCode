17161d70d43ffc738384ea7bc62803f2a4178656
Ankit Kumar Singh <112339860+BinaryMonkAnkit@users.noreply.github.com>
2026-07-30 17:15:39 +0530

    Fixes .env, allowed origins

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Setup CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sapienscode.pages.dev"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
