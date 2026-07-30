d25d8705bac947cb71270f16aea9941a96bdcbfb
Ankit Kumar Singh <112339860+BinaryMonkAnkit@users.noreply.github.com>
2026-07-30 17:07:45 +0530

    Revert "Fixes .env files, allowed origins"
    This reverts commit 5b90a802433010d8b9f6f94fd8e9273ed1d3b929.

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # Setup CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sapienscode.pages.dev"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
